4074680d417e58ecdb40e96aa0964ccf65084bc4
404-01 <4416552566367749@mail.ru>
2022-09-02 00:11:04 +0300

    Решил задание

--- test_2_2.py
+++ test_2_2.py
@@ -3,14 +3,18 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
+browser.config.hold_browser_open = True
+
 @pytest.fixture()
 def open_browser():
-    browser.config.window_height = 480
-    browser.config.window_width = 640
+    browser.config.window_height = 720
+    browser.config.window_width = 1280
     browser.open('https://google.com')
-    yield
 
-
-def search_site(open_browser):
+def test_search_site(open_browser):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+def test_nosearch_site(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('fdkgmifdg8sdksdfjks').press_enter()
+    browser.element('[id="search"]').should(have.no.text('Other text no search'))
